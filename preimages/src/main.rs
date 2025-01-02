57979e5ade4f38631d96ac3915913cf3bccbc522
Ignacio Hagopian <jsign.uy@gmail.com>
2025-01-02 17:04:51 -0300

    preimages: use mpt dfs walker to dump preimages
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- preimages/src/main.rs
+++ preimages/src/main.rs
@@ -1,8 +1,15 @@
-use std::path::Path;
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    path::Path,
+};
 
 use anyhow::{Context, Result};
 use clap::{command, Parser};
-use reth_db::mdbx::{tx::Tx, DatabaseArguments};
+use mptdfs::{MptDfsItem, MptDfsIterator};
+use reth_db::mdbx::{tx::Tx, DatabaseArguments, RO};
+
+mod mptdfs;
 
 #[derive(Parser)]
 #[command(name = "report")]
@@ -43,8 +50,29 @@ fn main() -> Result<()> {
     let tx = Tx::new(tx);
 
     match cli.subcmd {
-        SubCommand::Generate { path } => unimplemented!("generate"),
+        SubCommand::Generate { path } => generate(tx, &path)?,
     }
 
+    Ok(())
+}
+
+fn generate(tx: Tx<RO>, path: &str) -> Result<()> {
+    let mut f = File::create(path)?;
+    let mut writer = BufWriter::new(&mut f);
+    let it = MptDfsIterator::new(tx)?;
+    for entry in it {
+        match entry {
+            MptDfsItem::Account(address) => {
+                writer
+                    .write_all(address.as_slice())
+                    .context("writing address preimage")?;
+            }
+            MptDfsItem::StorageSlot(_, key) => {
+                writer
+                    .write_all(key.as_slice())
+                    .context("writing storage slot preimage")?;
+            }
+        }
+    }
     Ok(())
 }
